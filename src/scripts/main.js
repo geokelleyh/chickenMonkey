d4b3a170040b782c2f4f61b62f57fb3dbfda2f31
George Kelley <geokelley@gmail.com>
2019-09-09 13:40:06 -0500

    final commit---I hope

---
+++ src/scripts/main.js
@@ -0,0 +1,14 @@
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+    // Divide the current number by 2, and check if the remainder is 0
+    if (currentNumber % 7 === 0 && currentNumber % 2 ===0) {
+        console.log(currentNumber, "is = ChickenMonkey") // all multipes of 7 and 2 should appear
+    }
+    else if (currentNumber % 7 === 0) {
+        console.log(currentNumber, "is = Monkey") // all multipes of 7 should appear
+    }
+    else if (currentNumber % 2 === 0) {
+        console.log(currentNumber, "is = Chicken") // all even numbers should appear
+    }
+    else console.log(currentNumber)
+}
+
