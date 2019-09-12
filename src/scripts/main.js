e4d7e792ed966eedacd0d52e5b26b69751d45b09
George Kelley <geokelley@gmail.com>
2019-09-12 10:37:22 -0500

    corrected mistakes, replaced 2 with 5 on ChickenMonkey

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
     // Divide the current number by 2, and check if the remainder is 0
-    if (currentNumber % 7 === 0 && currentNumber % 2 ===0) {
+    if (currentNumber % 7 === 0 && currentNumber % 5 ===0) {
         console.log(currentNumber, "is = ChickenMonkey") // all multipes of 7 and 2 should appear
     }
     else if (currentNumber % 7 === 0) {
         console.log(currentNumber, "is = Monkey") // all multipes of 7 should appear
     }
-    else if (currentNumber % 2 === 0) {
+    else if (currentNumber % 5 === 0) {
         console.log(currentNumber, "is = Chicken") // all even numbers should appear
     }
     else console.log(currentNumber)
